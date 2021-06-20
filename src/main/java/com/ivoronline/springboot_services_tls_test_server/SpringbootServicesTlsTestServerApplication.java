65da9254798368579977ca6c1d6162df5fb1b09a
igoreta <ivoronline@gmail.com>
2021-06-20 06:29:59 +0200

    Initial commit

---
+++ src/main/java/com/ivoronline/springboot_services_tls_test_server/SpringbootServicesTlsTestServerApplication.java
@@ -0,0 +1,13 @@
+package com.ivoronline.springboot_services_tls_test_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootServicesTlsTestServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringbootServicesTlsTestServerApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/ivoronline/springboot_services_tls_test_server/SpringbootServicesTlsTestServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivoronline.springboot_services_tls_test_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootServicesTlsTestServerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
